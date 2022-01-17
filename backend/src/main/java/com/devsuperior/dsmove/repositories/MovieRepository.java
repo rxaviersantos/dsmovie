e3903114422e15ae87cd8565913d91398e8eef3c
Rodrigo <rodrigomxsantos@gmail.com>
2022-01-16 22:18:05 -0300

    Find movies

---
+++ backend/src/main/java/com/devsuperior/dsmove/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dsmove.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsmove.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+}
